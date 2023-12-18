e6f508cfe2e7317fbbefd23e4d1796e8d0cf13b2
“hanle” <hanle@go-goal.com>
2023-12-18 18:46:33 +0800

    update:insertion

---
+++ 排序/插入排序.js
@@ -0,0 +1,13 @@
+const arr = [12, 232, 234, 34, 43, 343, 434, 4, 12, 34, 43, 4, 4];
+const insertionSort = (arr) => {
+  const len = arr.length
+  for (let i = 1; i < len; i++) {
+   let par = i -1
+   let current = arr[i]
+   while (par >= 0 && ) {
+    
+   }
+  }
+  return arr
+};
+console.log(bubbleSort(arr));
