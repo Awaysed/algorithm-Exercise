e5b6d3ef89038342acd3e77eb112c41b10b63de5
Awaysed <122900171@qq.com>
2024-09-20 17:21:09 +0800

    stck3

---
+++ 数据结构/简化路径(栈).js
@@ -0,0 +1,16 @@
+https://leetcode.cn/problems/simplify-path/description/
+//  力扣↑
+var simplifyPath = function(path) {
+    const stack = []
+    let str = ''
+    const arr = path.split('/')
+    arr.forEach(val => {
+        if(val && val === '..'){
+            stack.pop()
+        }else if(val && val !== '.'){
+            stack.push(val)
+        }
+    });
+    str = stack.length > 0 ? '/' + stack.join('/'):'/'
+    return str
+};
