e274e57fc515237df1d1e1c1128aa224d7e09151
Awaysed <122900171@qq.com>
2025-01-02 17:59:35 +0800

    add:sd

---
+++ 力扣/删除排序数组中的重复项.js
@@ -0,0 +1,22 @@
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    const len  = nums.length
+    if(len===0) return 0
+    let fast = 1
+    let slow = 1
+    while (fast < len) {
+        if(nums[fast - 1] !== nums[fast]){
+            nums[slow] = nums[fast]
+            slow++
+        }
+        fast++
+    }
+    return slow
+};
+// 双指针 前面指针判断 上一个数和下一个数不相等就 将前指针赋慢指针 赋值  相等就跳过
