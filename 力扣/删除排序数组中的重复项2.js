e0b49f3b3ebb2fbf4f14a4abebedfeeb3e513da5
Awaysed <122900171@qq.com>
2025-01-03 15:03:31 +0800

    add:arr

---
+++ 力扣/删除排序数组中的重复项2.js
@@ -0,0 +1,21 @@
+https://leetcode.cn/problems/remove-duplicates-from-sorted-array-ii/description/?envType=study-plan-v2&envId=top-interview-150
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    const len = nums.length
+    if(len < 2) return  len
+    let fast = 2
+    let slow = 2
+    while (fast < len) {
+        if(nums[slow -2] !== nums[fast]){
+            nums[slow] = nums[fast]
+            slow++
+        }
+    }
+    fast++
+    return slow
+};
+// 好难 虽然代码能写下来 但是完全理解不了 太抽象 无法想象
