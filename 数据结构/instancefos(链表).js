92486abd0b7cdbfdf1888372d18b4724d7171527
Awaysed <122900171@qq.com>
2025-01-12 18:08:00 +0800

    update:ww

--- 数据结构/instancefos(链表).js
+++ 数据结构/instancefos(链表).js
@@ -1,8 +1,9 @@
 // instanceof 是 JavaScript 中的一个运算符，用于检查一个对象是否是某个类或其子类的实例。
-const instanceofs = (target,obj)=>{
+const instanceofs = (target,ConstructorFoo )=>{
     let p  = target
+    // ConstructorFoo构造函数
     while (p) {
-        if(p == obj.prototype){
+        if(p == ConstructorFoo.prototype){
             return true
         }
         p = p.__proto__ // 沿着原型链向上寻找
