e872f83b8b7970db926c680c2497316fef4bbb9b
Awaysed <122900171@qq.com>
2025-01-29 00:00:12 +0800

    add:axios2

--- 手写源码/axios原理.js
+++ 手写源码/axios原理.js
@@ -1,4 +1,11 @@
 class Axios {
+  constructor(){
+    // 拦截器储存
+    this.interceptors = {
+      request:[],
+      response:[]
+    }
+  }
   // 主请求方法
   request(config) {
     return new Promise((resolve, reject) => {
@@ -8,6 +15,9 @@ class Axios {
       Object.keys(params).forEach(key => {
         queryString += `${key}=${params[key]}&`
       })
+      if(method == 'GET'){
+        url += '?' + queryString.substring(0,queryString.length - 1)
+      }
       // 创建xhr对象
       const xhr = new XMLHttpRequest();
       // 初始化
@@ -31,15 +41,43 @@ class Axios {
               config,
             };
             let finalResponse = response;
+            // 响应拦截器
+            for (const interceptor of this.interceptors.response) {
+              finalResponse = interceptor(finalResponse)
+            }
             resolve(finalResponse);
           }
         } else {
           reject(new Error(`Request failed with status ${xhr.status}`));
         }
       };
+      //请求拦截器
+      let finalConfig = config
+      for (const iterator of this.interceptors.request) {
+        finalConfig = iterator(finalConfig)
+      }
       // 发送请求
       const body = JSON.stringify(finalConfig.data);
       xhr.send(body);
     });
+  }
+  // get
+  get(url,params,config){
+    return this.request({
+      ... config,
+      url,
+      params,
+      method:"GET",
+    })
+  }
+  post(url,data,config){
+    return this.request(
+      {
+        ...config,
+        url,
+        data,
+        method:"POST"
+      }
+    )
   }
 }
