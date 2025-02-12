43c0090e976b56c953eb6123570a98f6aca4dde0
Awaysed <122900171@qq.com>
2025-02-13 00:13:02 +0800

    update:2

--- 手写源码/promise.js
+++ 手写源码/promise.js
@@ -11,38 +11,68 @@ obj.then((res) => {
 
 class MyPromise {
   constructor(executor) {
-    (this.state = "pending"), // 状态
-      (this.value = undefined), // 成功值
-      (this.onFulfilledCallBacks = []); // 成功的回调
+    this.state = "pending"; // 状态
+    this.value = undefined; // 成功值
+    this.onFulfilledCallBacks = []; // 成功的回调
     this.onRejectedCallBacks = []; // 失败回调
     const resolve = (value) => {
       if (this.state === "pending") {
         setTimeout(() => {
           this.state = "fulfilled";
           this.value = value;
-          this.onFulfilledCallBacks.forEach((foo)=>{
-            foo(value)
-          })
+          this.onFulfilledCallBacks.forEach((foo) => {
+            foo(value);
+          });
         });
       }
     };
     const reject = (value) => {
-        if (this.state === "pending") {
-            setTimeout(() => {
-              this.state = "rejected";
-              this.value = value;
-              this.onRejectedCallBacks.forEach((foo)=>{
-                foo(value)
-              })
-            });
-          }
+      if (this.state === "pending") {
+        setTimeout(() => {
+          this.state = "rejected";
+          this.value = value;
+          this.onRejectedCallBacks.forEach((foo) => {
+            foo(value);
+          });
+        });
+      }
     };
     // 执行回调
     try {
       executor(resolve, reject);
     } catch (error) {}
   }
-  then(){
-    
+  then(onFulfilled, onRejected) {
+    return new MyPromise((resolve, reject) => {
+      const fulfilledHandler = () => {
+        try {
+          const result = onFulfilled ? onFulfilled(this.value) : this.value;
+          result instanceof MyPromise
+            ? result.then(resolve, reject)
+            : resolve(result);
+        } catch (error) {
+          reject(error);
+        }
+      };
+      const rejectedHandler = () => {
+        try {
+          const result = onRejected ? onRejected(this.reason) : this.reason;
+          result instanceof MyPromise
+            ? result.then(resolve, reject)
+            : reject(result);
+        } catch (error) {
+          reject(error);
+        }
+      };
+      // 状态
+      if (this.state === "fulfilled") {
+        setTimeout(fulfilledHandler);
+      } else if (this.state === "rejected") {
+        setTimeout(rejectedHandler);
+      } else {
+        this.onFulfilledCallBacks.push(fulfilledHandler);
+        this.onRejectedCallBacks.push(onRejected);
+      }
+    });
   }
 }
