cb44c926cebaed635542656edf5845e0b8b8e870
Awaysed <122900171@qq.com>
2025-07-30 09:48:48 +0800

    test:enjoy

---
+++ Z-日常碎碎念/7月/doctor.js
@@ -0,0 +1,21 @@
+pages / index / index;
+consultation / home / home;
+outpatient / basicHome / basicHome;
+outpatient / upHome / upHome;
+pages / webView / webView;
+pages / billOrder / billOrderList / billOrderList;
+mdtConsultation / home / home;
+workbench / myPlusSource / myPlusSource;
+pages / mine / mine;
+pages / advisory / my - advisory / index;
+workbench / refundAuditList / refundAuditList;
+
+
+
+// 新加
+workbench/needHelp/needHelp;pages/setting/index;consultation/setMessage/setMessage;workbench/announcement/announcement;mySelfCard/doctorDetail/doctorDetail;
+pages/userDetail/userDetail;
+pages/notice/notice;
+mySelfCard/doctorDetail/doctorDetail;
+pages/evaluateList/evaluateList;
+pages/businessStatistics/businessStatistics;
